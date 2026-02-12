7dd7597007dfbe75d1f0d5dacd4c36db935fbd76
didericis <eric@dideric.is>
2026-02-12 12:31:27 -0500

    add tests for youtube embeds

--- test/index.test.js
+++ test/index.test.js
@@ -38,3 +38,19 @@ test("add heading links", async function (t) {
 <h2 id="user-content-two-1"><a data-heading-link href="#two-1">#</a>Two</h2>`,
   );
 });
+
+test("embed youtube", async function (t) {
+  const markdown = `
+[![Build with Pear - Episode 01: Developing with Pear](https://img.youtube.com/vi/y2G97xz78gU/0.jpg)](https://www.youtube.com/watch?v=y2G97xz78gU)
+`;
+  const result = await markdownToHtml(markdown);
+  t.is(
+    result,
+    `\
+<p>
+  <div class="youtube-video-player">
+    <iframe title="Youtube video player" class="youtube-video-player__video" src="https://www.youtube-nocookie.com/embed/y2G97xz78gU?rel=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+  </div>
+</p>`,
+  );
+});
